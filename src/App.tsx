1e127546f9fee8e761f91dd9e9a23da9cc0ce564
Soham Das <publicsoham.24@gmail.com>
2023-04-26 22:33:10 +0530

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+/* eslint-disable eol-last */
 
-function App() {
+import React from 'react'
+import type { ReactElement } from 'react'
+
+const App: React.FC = (): ReactElement => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <div className='text-3xl font-bold underline'>App</div>
+  )
 }
 
-export default App;
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/* eslint-disable eol-last */
+/* eslint-disable indent */
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        './src/**/*.{js,jsx,ts,tsx}'
+    ],
+    theme: {
+        extend: {}
+    },
+    plugins: []
+}
